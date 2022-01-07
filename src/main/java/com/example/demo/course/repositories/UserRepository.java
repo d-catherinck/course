d506dbc8390b1059125659c7aa0175bb3eaa3db2
d-catherinck <d.catherinck@gmail.com>
2022-01-07 08:34:03 -0300

    Service layer, component registration

--- src/main/java/com/example/demo/course/repositories/UserRepository.java
+++ src/main/java/com/example/demo/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
